de5eda40b53a1abee763b715137f6468b642d0be
joaoambrozetto <ambrozetto@gmail.com>
2025-12-17 01:39:18 -0300

    Change all file and folder structure

--- main.py
+++ main.py
@@ -1,19 +1,73 @@
+import numpy as np
 import os
 import pandas as pd
+import requests
 
-from get_data import get_data_from_csv
-from medallion import create_layer
+from dotenv import load_dotenv
 
-PATH_LAYERS = './data'
-PATH_CEP = './data/raw_data/lista_ceps.csv'
+# Carregar variáveis de ambiente
 
-# creating folders bronze_layer, silver_layer and gold_layer:
+load_dotenv()
 
-create_layer(['bronze', 'silver', 'gold'], PATH_LAYERS)
+TOKEN = os.environ['TOKEN']
 
-# populating bronze layer with cep's list:
+# Acessar e ler os dados do arquivo csv, criando um dataframe temporário
 
-df = get_data_from_csv(PATH_CEP)
+CSV_PATH = './data/raw_data/lista_ceps.csv'
 
-if 'CEP_bronze.csv' not in os.listdir('./data/bronze_layer'):
-    df.to_csv('./data/bronze_layer/CEP_bronze.csv', index=False)
+temp_df = pd.DataFrame(pd.read_csv(CSV_PATH))
+
+# Formatar o CEP de '00000-000' para '00000000' para usar na requisição
+
+for x in range(temp_df.size):
+    temp_df.loc[x] = temp_df.loc[x][:5] + temp_df.loc[x][6:]
+
+# Criar um dataframe vazio para popular com as requisições:
+
+columns = {'altitude': None,
+           'cep': None,
+           'latitude': None,
+           'longitude': None,
+           'logradouro': None,
+           'bairro': None,
+           'complemento': None,
+           'cidade': None,
+           'estado': None,
+           'ddd': None,
+           'ibge': None}
+df = pd.DataFrame(data=columns, index=[])
+
+# Fazer a requisição para a API CEP Aberto
+
+CEP = '13481164'
+URL = f'https://www.cepaberto.com/api/v3/cep?cep={CEP}'
+headers = {'Authorization': f'Token token={TOKEN}'}
+response = requests.get(URL, headers=headers)
+
+# Inserir dados do response no DataFrame:
+data = response.json()
+if 'complemento' not in data.keys():
+    df.loc[0] = [data['altitude'],
+                 data['cep'],
+                 data['latitude'],
+                 data['longitude'],
+                 data['logradouro'],
+                 data['bairro'],
+                 np.nan,
+                 data['cidade']['nome'],
+                 data['estado']['sigla'],
+                 data['cidade']['ddd'],
+                 data['cidade']['ibge']]
+else:
+    df.loc[0] = [data['altitude'],
+                 data['cep'],
+                 data['latitude'],
+                 data['longitude'],
+                 data['logradouro'],
+                 data['bairro'],
+                 data['complemento'],
+                 data['cidade']['nome'],
+                 data['estado']['sigla'],
+                 data['cidade']['ddd'],
+                 data['cidade']['ibge']]
+print(df)
